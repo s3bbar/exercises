d7f4338bff19addbfe3a7b9cc6f97b94522f2cd1
s3bbar <seba.baranowski@gmail.com>
2023-06-20 23:54:21 +0200

    Scopes of function

---
+++ main.py
@@ -0,0 +1,13 @@
+multiplier = 5
+has_calculated = False
+
+
+def multiply_calculator(number):
+    global has_calculated
+    has_calculated = True
+    result = number * multiplier
+    return result
+
+
+print(multiply_calculator(10))
+print(has_calculated)

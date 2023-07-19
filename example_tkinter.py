9a22094a8845131ca8960fd7ab184dae7979e06b
s3bbar <seba.baranowski@gmail.com>
2023-07-19 20:14:36 +0200

    map function

---
+++ example_tkinter.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import ttkbootstrap as tb
+
+root = tb.Window(themename='superhero')
+root.title("TTK Bootstrap!")
+root.geometry("500x350")
+
+#Style
+
+my_style = tb.Style()
+my_style.configure('success.Outline.TButton', font=('Helvetica', 18))
+
+my_button = tb.Button(text="Hello World!", bootstyle="success", style='success.Outline.TButton', width=20)
+my_button.pack(pady=30)
+
+for style in my_style.theme_names():
+    print(style)
+
+
+root.mainloop()
